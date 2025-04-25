44bed8511eeb355ba05d85aec53f5445f635d0fc
michaelpginn <milespergallon271@gmail.com>
2025-04-25 15:03:18 -0500

    Add log

--- src/training/train.py
+++ src/training/train.py
@@ -60,6 +60,7 @@ def train(
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             scaler.step(optimizer)
             scaler.update()
+            print("Loss", loss.detach().item())
             train_loss += loss.detach().item() / len(train_dataloader)
             pbar.update()
 
