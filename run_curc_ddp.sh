0ade2f2ce00c34afc4b44de218803bbcbc83ea85
Michael Ginn <michaelginn@Michaels-MacBook-Pro-3.local>
2025-05-18 18:18:59 -0400

    Fix script

--- run_curc_ddp.sh
+++ run_curc_ddp.sh
@@ -12,19 +12,19 @@
 #SBATCH --error=logs/%j.err
 
 export MASTER_PORT=$((10000 + SLURM_JOB_ID % 50000))
-
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-echo "WORLD_SIZE="$WORLD_SIZE
 
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr
-echo "MASTER_ADDR="$MASTER_ADDR
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 
 module purge
 module load miniforge
 mamba activate polygloss
 cd "/projects/$USER/polygloss"
 
-
-srun python run.py "$1"
-
+torchrun \
+  --nproc_per_node=$SLURM_NTASKS_PER_NODE \
+  --nnodes=$SLURM_NNODES \
+  --node_rank=$SLURM_NODEID \
+  --master_addr=$MASTER_ADDR \
+  --master_port=$MASTER_PORT \
+  run.py "$1"
