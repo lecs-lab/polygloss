Fix perplexity normalization

src/perplexity.py
@@ -99,13 +99,13 @@ def calculate_ppl(
                 batch = batch.to(device)
                 out = model(**batch)
                 
-                bs = batch["labels"].size(0)
                 loss = _get_loss(out, batch["labels"]).item()
-                local_eval_loss_sum += loss * bs
+                batch_tokens = (batch["labels"] != -100).sum().item()
+                local_total_tokens += batch_tokens
+                local_eval_loss_sum += loss * batch_tokens
                 
                 # Count tokens, morphemes, and words
                 labels = batch["labels"]
-                local_total_tokens += (labels != -100).sum().item()
                 decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
                 for decoded_text in decoded_labels:
                     local_total_morphemes += len(re.split(boundary_pattern, decoded_text))
@@ -148,9 +148,9 @@ def calculate_ppl(
     for glottocode in glottocodes:
         loss = eval_loss_per_language[glottocode]
         ppl_per_language[glottocode] = {
-            "ppl_per_token": torch.exp(torch.tensor(loss)).item(),
-            "ppl_per_morpheme": torch.exp(torch.tensor(loss)).item() * tokens_per_language[glottocode] / morphemes_per_language[glottocode],
-            "ppl_per_word": torch.exp(torch.tensor(loss)).item() * tokens_per_language[glottocode] / words_per_language[glottocode],
+            "ppl_per_token": torch.exp(torch.tensor(loss)).item() / tokens_per_language[glottocode],
+            "ppl_per_morpheme": torch.exp(torch.tensor(loss)).item() / morphemes_per_language[glottocode],
+            "ppl_per_word": torch.exp(torch.tensor(loss)).item() / words_per_language[glottocode],
         }
     
     return ppl_per_language