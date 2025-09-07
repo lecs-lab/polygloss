14c1d8af356c8e27b38d16c487e7449a036fa1f5
Michael Ginn <migi8081@login-ci5.rc.colorado.edu>
2025-09-07 00:39:24 -0600

    Switch to clearlab

--- run_curc_ddp.sh
+++ run_curc_ddp.sh
@@ -5,9 +5,9 @@
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --cpus-per-task=4
 #SBATCH --time=7-00:00:00
-#SBATCH --qos=blanca-curc-gpu
-#SBATCH --partition=blanca-curc-gpu
-#SBATCH --account=blanca-curc-gpu
+#SBATCH --qos=blanca-clearlab1
+#SBATCH --partition=blanca-clearlab1
+#SBATCH --account=blanca-clearlab1
 #SBATCH --out=logs/%j.log
 #SBATCH --error=logs/%j.log
 
