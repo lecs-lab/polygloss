47e9373ec6c1f7cf0899ef4acc52a6143d515013
Michael Ginn <migi8081@login-ci3.rc.colorado.edu>
2025-09-12 15:30:51 -0600

    Use h100s

--- run_curc_ddp.sh
+++ run_curc_ddp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=3
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:h100:3
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --cpus-per-task=4
 #SBATCH --time=7-00:00:00
