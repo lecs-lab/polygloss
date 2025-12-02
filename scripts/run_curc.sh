a5e9258a46c944f78683a8ae3091480ec48f611c
Michael Ginn <michaelginn00@gmail.com>
2025-12-02 13:33:03 -0800

    Use a100s

--- scripts/run_curc.sh
+++ scripts/run_curc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=10
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=3
+#SBATCH --gres=gpu:a100:1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --cpus-per-task=4
 #SBATCH --time=7-00:00:00
