510122d1fb71694a89965f5dc5d519f05a663768
Michael Ginn <mginn@gh-login02.delta.ncsa.illinois.edu>
2025-12-20 16:05:25 -0600

    Log offending params

--- scripts/run_delta_ddp.sh
+++ scripts/run_delta_ddp.sh
@@ -39,6 +39,7 @@ export MASTER_PORT=$((10000 + SLURM_JOB_ID % 50000))
 export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export OMP_NUM_THREADS=4
 export PYTHONUNBUFFERED=1
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
 
 cd "/projects/bebe/$USER/polygloss"
 
