a29f085ec5a1e5cb613d1b709c59598d5e2c1a50
Michael Ginn <michaelginn00@gmail.com>
2025-12-29 00:24:14 -0600

    Add grpo skeleton

---
+++ src/grpo.py
@@ -0,0 +1,10 @@
+def grpo(
+    model,
+    tokenizer,
+    train_dataloader: DataLoader,
+    dev_dataloader: DataLoader,
+    config: ExperimentConfig,
+    models_folder: pathlib.Path,
+    distributed_parameters: DistributedParameters,
+):
+    pass
