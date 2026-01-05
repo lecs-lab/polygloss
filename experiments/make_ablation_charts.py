ca566118dee3acf186fd0ded6f181ab2e8c6d8d1
Michael Ginn <michaelginn00@gmail.com>
2026-01-05 09:02:33 -0600

    Add grpo

--- experiments/make_ablation_charts.py
+++ experiments/make_ablation_charts.py
@@ -207,3 +207,68 @@ plt.savefig(
 )
 plt.show()
 plt.close(f)
+
+
+# GRPO
+f, ax = plt.subplots(figsize=(4, 1.4))
+data = df[df["Method"].isin(["PolyGloss [concat]", "GRPO"])]
+data = data[data["Language"] == "git"]
+data["Method"] = data["Method"].replace(
+    {"PolyGloss [concat]": "PolyGloss", "GRPO": "+ GRPO"}
+)
+# summary = df.groupby(["Method", "Metric"], as_index=False).agg(
+#     Mean=("Score", "mean"),
+#     sem=("Score", "sem"),
+# )
+ax.tick_params(
+    axis="x",
+    which="both",
+    bottom=True,
+    top=False,
+    length=4,
+    width=1,
+    labelsize=9,
+)
+g = sns.barplot(
+    data=data,
+    x="Score",
+    y="Metric",
+    hue="Method",
+    palette="deep",
+    edgecolor="black",
+    # estimator="mean",
+    # errorbar="se",
+    linewidth=1,
+    alpha=1,
+    ax=ax,
+    # legend=None,
+)
+leg = ax.legend(
+    loc="lower left",
+    bbox_to_anchor=(0, 1.02),  # x=0 is the *axes* left edge
+    bbox_transform=ax.transAxes,  # <-- crucial
+    ncol=2,
+    frameon=False,
+    title=None,
+    borderaxespad=0.0,
+    columnspacing=1.2,
+    handletextpad=0.6,
+)
+
+# Make the legend columns left-aligned internally
+leg._legend_box.align = "left"
+sns.despine(
+    bottom=False,
+)
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontweight("bold")
+ax.set_xlabel("Mean Score", fontweight="bold")
+ax.set(ylabel=None)
+ax.set_xlim(0, 1)
+for text in ax.legend_.get_texts():
+    text.set_fontweight("bold")
+plt.savefig(
+    "experiments/viz/grpo.pdf", format="pdf", bbox_inches="tight", pad_inches=0.01
+)
+plt.show()
+plt.close(f)
