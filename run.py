00d5261628ede261c6261cd3dac080cd24e7100d
Michael Ginn <michaelginn00@gmail.com>
2026-01-23 14:31:45 -0700

    Move logging to rank 0

--- run.py
+++ run.py
@@ -196,22 +196,21 @@ def run(
         config=config,
         distributed_parameters=distributed_parameters,
     )
-    assert predictions is not None
-    assert perplexity_by_lang is not None
-    # Join with original dataset to add language info
-    meta = (
-        dataset["test"]  # type:ignore
-        .to_pandas()[["id", "glottocode"]]
-        .drop_duplicates(subset=["id"])
-    )
-    predictions_with_langs = predictions.merge(
-        meta,
-        on="id",
-        how="left",
-    )
-
     if distributed_parameters["rank"] == 0:
         assert predictions is not None
+        assert perplexity_by_lang is not None
+        # Join with original dataset to add language info
+        meta = (
+            dataset["test"]  # type:ignore
+            .to_pandas()[["id", "glottocode"]]
+            .drop_duplicates(subset=["id"])
+        )
+        predictions_with_langs = predictions.merge(
+            meta,
+            on="id",
+            how="left",
+        )
+
         # Join with original dataset to add language info
         meta = (
             dataset["test"]  # type:ignore
